4a26dff80315bc6426317b20883f4eb1d0268147
Deivi Peña <systemson@gmail.com>
2019-05-02 16:03:13 -0400

    Updates

--- framework/Commands/ServerCommand.php
+++ framework/Commands/ServerCommand.php
@@ -10,22 +10,41 @@ use Carbon\Carbon;
 
 class ServerCommand extends Command
 {
-    // the name of the command (the part after "bin/console")
     protected static $defaultName = 'run';
 
+    /**
+     * Configures the current command.
+     */
+    protected function configure()
+    {
+        $this->addOption('explorer', 'e', InputArgument::OPTIONAL, 'The explorer to open.');
+    }
+
+
+    /**
+     * Executes the current command.
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
         $phpVersion = phpversion();
-        $now = Carbon::now();
+
+        $scheme = 'http';
+        $host = 'localhost';
         $port = '3000';
+        $url = "{$scheme}://{$host}:{$port}";
+
+        $now = Carbon::now();
+
+        $publicFolder = 'public';
+
+        Open::open($url, $input->getOption('explorer'));
 
         $output->writeln("PHP {$phpVersion} Development Server started at {$now}");
-        $output->writeln("Listening on http://localhost:{$port}");
+        $output->writeln("Listening on {$url}:{$port}");
         $output->writeln("Amber Framework v-beta.");
         $output->writeln("Never use this server on production.");
         $output->writeln("Press Ctrl-C to quit.");
         
-        exec('php -S localhost:3000 -t public');
+        exec("php -S {$host}:{$port} -t {$publicFolder}");
     }
 }
