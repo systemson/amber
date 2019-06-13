e46946412a6be4a696de03c1fefbd4464d0dc2ac
Deivi Peña <systemson@gmail.com>
2019-06-13 15:52:22 -0400

    Updates

--- config/routes.php
+++ config/routes.php
@@ -13,12 +13,17 @@ return [
                 'Amber\Framework\Http\Server\Middleware\AuthMiddleware',
                 'Amber\Framework\Http\Server\Middleware\CsfrMiddleware',
             ],
+            'prefix' => '',
+            'namespace' => 'App\Http\Controllers',
         ],
 
         'api' => [
             'path' => CONFIG_DIR . '/routes/api.php',
             'middlewares' => [
+                'Amber\Framework\Http\Server\Middleware\ApiTokenMiddleware',
             ],
+            'prefix' => 'api',
+            'namespace' => 'App\Http\Controllers\Api',
         ],
 
     ],
