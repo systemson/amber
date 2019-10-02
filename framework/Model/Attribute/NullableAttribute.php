30645b3a372f8c3b4ac1bbc4dd44516cc64d8c5a
Deivi Peña <systemson@gmail.com>
2019-10-02 14:02:51 -0400

    Bugfix

--- framework/Model/Attribute/NullableAttribute.php
+++ framework/Model/Attribute/NullableAttribute.php
@@ -6,8 +6,4 @@ use Amber\Utils\Implementations\AbstractNullObject;
 
 class NullableAttribute extends AbstractNullObject
 {
-    public function __call($method, $args)
-    {
-        return;
-    }
 }
