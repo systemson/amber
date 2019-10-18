bbf4bf363b9771098379374712677d837682e6cc
Deivi Peña <systemson@gmail.com>
2019-10-18 14:48:45 -0400

    Updates

---
+++ framework/Model/Provider/RelationProvider.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Amber\Model\Provider;
+
+use Aura\SqlQuery\Common\SelectInterface;
+
+/**
+ *
+ */
+class RelationProvider
+{
+    /**
+     * @var SelectInterface
+     */
+    protected $query;
+
+    /**
+     * @var PROPTYPE
+     */
+    protected $provider;
+
+    /**
+     * @var PROPTYPE
+     */
+    protected $pkey;
+
+    /**
+     * @var PROPTYPE
+     */
+    protected $fkey;
+
+    /**
+     * @var PROPTYPE
+     */
+    protected $multiple = false;
+
+    public function __construct(
+        AbstractProvider $provider,
+        SelectInterface $query,
+        string $pkey,
+        string $fkey,
+        bool $multiple = false
+    ) {
+        $this->setProvider($provider);
+        $this->setQuery($query);
+        $this->setPkey($pkey);
+        $this->setFkey($fkey);
+        $this->setMultiple($multiple);
+    }
+
+    /**
+     * @return SelectInterface
+     */
+    public function getQuery(): SelectInterface
+    {
+        return $this->query;
+    }
+
+    /**
+     * @param SelectInterface $query
+     */
+    public function setQuery(SelectInterface $query): self
+    {
+        $this->query = $query;
+
+        return $this;
+    }
+
+    /**
+     * @return AbstractProvider
+     */
+    public function getProvider(): AbstractProvider
+    {
+        return $this->provider;
+    }
+
+    /**
+     * @param AbstractProvider $provider
+     */
+    public function setProvider(AbstractProvider $provider): self
+    {
+        $this->provider = $provider;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPkey(): string
+    {
+        return $this->pkey;
+    }
+
+    /**
+     * @param string $pkey
+     */
+    public function setPkey(string $pkey): self
+    {
+        $this->pkey = $pkey;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFkey(): string
+    {
+        return $this->fkey;
+    }
+
+    /**
+     * @param string $fkey
+     */
+    public function setFkey(string $fkey): self
+    {
+        $this->fkey = $fkey;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getMultiple(): bool
+    {
+        return $this->multiple;
+    }
+
+    /**
+     * @param bool $multiple
+     */
+    public function setMultiple(bool $multiple = false): self
+    {
+        $this->multiple = $multiple;
+
+        return $this;
+    }
+}
